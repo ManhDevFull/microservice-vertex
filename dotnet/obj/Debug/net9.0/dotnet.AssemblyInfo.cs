a15c48304091f01c89fe6f513bafc0a6bcd91aae
Le Khac Hieu <lekhachieupt@gmail.com>
2025-11-25 21:47:03 +0700

    commit afte pull origin develop

--- dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337f3c50ea813517e90e1dd2cf24129c526ddc69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6645911ad9f175f5c10f6d5782f38d9ea2d4e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
