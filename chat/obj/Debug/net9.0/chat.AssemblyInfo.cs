a7126cb7fe278791c11e2de16082286848344fca
Le Khac Hieu <lekhachieupt@gmail.com>
2025-11-25 21:55:19 +0700

    commit before pull origin dev

--- chat/obj/Debug/net9.0/chat.AssemblyInfo.cs
+++ chat/obj/Debug/net9.0/chat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6645911ad9f175f5c10f6d5782f38d9ea2d4e24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15c48304091f01c89fe6f513bafc0a6bcd91aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
