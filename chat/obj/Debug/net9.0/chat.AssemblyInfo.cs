67c172335699c87e8d13730d5e61936828688f43
Le Khac Hieu <lekhachieupt@gmail.com>
2025-12-03 20:27:34 +0700

    pull success task payment from dev

--- chat/obj/Debug/net9.0/chat.AssemblyInfo.cs
+++ chat/obj/Debug/net9.0/chat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf5edb7afa78b7fe8698941740c6cfc9824bb3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea122ab53b33c267e96e35909c359ba34fa3ec27")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
