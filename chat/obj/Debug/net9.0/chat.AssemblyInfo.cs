eaef06bcdc8668a4564e8a59e4d9533b40cda11b
Le Khac Hieu <lekhachieupt@gmail.com>
2025-12-04 14:18:54 +0700

    commit to checkout user

--- chat/obj/Debug/net9.0/chat.AssemblyInfo.cs
+++ chat/obj/Debug/net9.0/chat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b59e99d87a3b413b044929d77d0e994fd914c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5db5ea77f1aa5a3fcdd23260aace8341e93e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
